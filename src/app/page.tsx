2692bc027502fd880f0a5ad3ed4bda8b116de319
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-05-12 16:53:00 -0300

    sidebar and main config initiated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,58 @@
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
 
 export default function Home() {
   return (
-    <main></main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <nav className='space-y-5'>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Daily Mix 1</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Daily Mix 2</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Daily Mix 3</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Daily Mix 4</a>
+            <a href='' className='text-sm text-zinc-400 hover:text-zinc-100'>Daily Mix 5</a>            
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+         <div className='flex items-center gap-4'>
+          <button className='rounded-full bg-black/40 p-1'>
+            <ChevronLeft />
+          </button>
+          <button className='rounded-full bg-black/40 p-1'>
+            <ChevronRight />
+          </button>
+         </div>
+
+         <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+
+         <div className='grid grid-cols-3 gap-4'>
+            <div className='bg-white/10 rounded'>Playlist</div>
+            <div className='bg-white/10 rounded'>Playlist</div>
+            <div className='bg-white/10 rounded'>Playlist</div>
+            <div className='bg-white/10 rounded'>Playlist</div>
+            <div className='bg-white/10 rounded'>Playlist</div>
+            <div className='bg-white/10 rounded'>Playlist</div>
+         </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
   )
 }
